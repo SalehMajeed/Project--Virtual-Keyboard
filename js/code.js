3f00746a86bd35b5f3fa323caffd79711b258b71
= <saleh.majeed8@gmail.com>
2020-11-01 07:25:33 +0530

    keys

--- js/code.js
+++ js/code.js
@@ -31,7 +31,114 @@ const keyboard = {
     document.body.appendChild(this.elements.main);
   },
 
-  _create_keys() {},
+  _create_keys() {
+    const fragment = document.createDocumentFragment();
+    const key_layout = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0',
+      'backspace',
+      'q',
+      'w',
+      'e',
+      'r',
+      't',
+      'y',
+      'u',
+      'i',
+      'o',
+      'p',
+      'caps',
+      'a',
+      's',
+      'd',
+      'f',
+      'g',
+      'h',
+      'j',
+      'k',
+      'l',
+      'enter',
+      'done',
+      'z',
+      'x',
+      'c',
+      'v',
+      'b',
+      'n',
+      'm',
+      ',',
+      '.',
+      '?',
+      'space',
+    ];
+
+    // Create HTML for an icon
+    const create_icon_html = (icon_name) => {
+      return '<i class="material-icon">${icon_name}</i>';
+    };
+
+    key_layout.forEach((key) => {
+      const key_element = document.createElement('button');
+      const insert_line_break =
+        ['backspace', 'p', 'enter', '?'].indexOf(key) !== -1;
+
+      // Add attributes/classes
+      key_element.setAttribute('type', 'button');
+      key_element.classList.add('keyboard__key');
+
+      switch (key) {
+        case 'backspace':
+          key_element.classList.add('keyboard__key--wide');
+          key_element.innerHTML = create_icon_html('backspace');
+
+          key_element.addEventListener('click', () => {
+            this.properties.value = this.properties.value.substring(
+              0,
+              this.properties.value.length
+            );
+            this._trigger_event('oninput');
+          });
+          break;
+
+        case 'caps':
+          key_element.classList.add(
+            'keyboard__key--wide',
+            'keyboard__key--activatable'
+          );
+          key_element.innerHTML = create_icon_html('keyboard_capslock');
+
+          key_element.addEventListener('click', () => {
+            this._toggle_caps_lock();
+            key_element.classList.toggle(
+              'keyboard__key--active',
+              this.properties.caps_lock
+            );
+          });
+          break;
+
+        case 'enter':
+          key_element.classList.add('keyboard__key--wide');
+          key_element.innerHTML = create_icon_html('keyboard_return');
+
+          key_element.addEventListener('click', () => {
+            this.properties.value += '\n';
+            key_element.classList.toggle(
+              'keyboard__key--active',
+              this.properties.caps_lock
+            );
+          });
+          break;
+      }
+    });
+  },
 
   _trigger_event(handler_name) {
     console.log('Event Triggered! Event Name: ' + handler_name);
